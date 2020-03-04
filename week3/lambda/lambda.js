3fe93335cc98f73e9cb309e3a8095259af850c4a
Dierk Koenig <dierk.koenig@canoo.com>
2020-03-04 10:23:43 +0100

    after week 3 de

--- week3/lambda/lambda.js
+++ week3/lambda/lambda.js
@@ -1,5 +1,19 @@
+const id = x => x;
+const fst = x => y => x;
+const snd = x => y => y;
+const M   = f => f (f);
 
+const konst = fst;
 
+const T = first => second => first;
+const F = first => second => second;
+
+const and = first => second => first ( second ) ( first );
+const or  = M ;
+
+const Pair = first => second => selector  => selector (first) (second);
+const firstname = fst;
+const lastname  = snd;
 
 // ----- special -----
 

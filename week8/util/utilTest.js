2b5c7fe824e2ab4cd182038b1992e8cd1fce452d
Dierk Koenig <dierk.koenig@canoo.com>
2020-04-08 09:28:22 +0200

    week 8 move test case to new testbstructure

--- week8/util/utilTest.js
+++ week8/util/utilTest.js
@@ -16,14 +16,12 @@ test("util-times1", assert => {
 }) ;
 
 
-( () => {
-    let ok = [];
+test("util-times2", assert => {
 
     const collect = (10).times( n => n+1 );
 
-    ok.push(collect.length === 10);
-    ok.push(collect[0] === 1);
-    ok.push(collect[9] === 10);
+    assert.equals(collect.length , 10);
+    assert.equals(collect[0]     , 1);
+    assert.equals(collect[9]     , 10);
 
-    report("util-times2", ok);
-}) ();
+});

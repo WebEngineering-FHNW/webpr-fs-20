8a4a374039f29ae661adef7e961b55bc76734680
Dierk Koenig <dierk.koenig@canoo.com>
2020-04-08 10:16:02 +0200

    week 8 exploring todolist with static table rows

--- week8/todo/todo.js
+++ week8/todo/todo.js
@@ -4,5 +4,12 @@ function startTodo() {
 }
 
 function addTodo() {
+    const container = document.getElementById("todoContainer");
+    container.innerHTML += `
+    <tr>
+        <td><input type="text" value="empty"></td>
+        <td><input type="checkbox"></td>
+    </tr>
+`
     
 }
